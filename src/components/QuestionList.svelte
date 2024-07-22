27983cc3e961b2ef440d3bbf7219ce8926a944ea
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2024-07-21 22:59:29 -0700

    fixed some QuestionList styling to center divs

--- src/components/QuestionList.svelte
+++ src/components/QuestionList.svelte
@@ -42,16 +42,18 @@
   }
 </style>
 
-<div id="question-list" class="question-list px-12 py-2">
-  {#each questions as question}
-    <h1
-      id={`question-${question}`}
-      class="font-sans text-xl text-gray-900 dark:text-gray-200 text-center p-5
-       transition-transform duration-300 ease-in-out cursor-pointer
-       {currentQuestion === question ? "text-2xl font-bold scale-150" : ""}"
-      on:click={() => questionNumber.set(question)}
-    >
-      {question}
-    </h1>
-  {/each}
+<div class="fixed bottom-0 w-full flex justify-center">
+  <div id="question-list" class="question-list px-12 py-2">
+    {#each questions as question}
+      <h1
+        id={`question-${question}`}
+        class="font-sans text-xl text-gray-900 dark:text-gray-200 text-center p-5
+          transition-transform duration-300 ease-in-out cursor-pointer
+          {currentQuestion === question ? 'text-2xl font-bold scale-150' : ''}"
+        on:click={() => questionNumber.set(question)}
+      >
+        {question}
+      </h1>
+    {/each}
+  </div>
 </div>
