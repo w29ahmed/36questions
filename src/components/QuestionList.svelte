c4e456d55836d1f5c3194089133334c9c68d1b87
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2024-07-21 23:32:19 -0700

    cleaned up styling furter in QuestionList, added padding even when not all question numbers fit on screen

--- src/components/QuestionList.svelte
+++ src/components/QuestionList.svelte
@@ -14,7 +14,9 @@
     const list = document.getElementById("question-list");
     const item = document.getElementById(`question-${currentQuestion}`);
     if (list && item) {
-      list.scrollLeft = item.offsetLeft - list.offsetWidth / 2 + item.offsetWidth / 2;
+      const outerWrapper = document.querySelector('.outer-wrapper');
+      const padding = parseFloat(getComputedStyle(outerWrapper).paddingLeft);
+      list.scrollLeft = item.offsetLeft - list.offsetWidth / 2 + item.offsetWidth / 2 - padding;
     }
   }
 
@@ -31,29 +33,44 @@
 </script>
 
 <style>
-  .question-list {
+  .outer-wrapper {
+    display: flex;
+    justify-content: center;
+    overflow-x: hidden;
+  }
+
+  .inner-wrapper {
     display: flex;
     overflow-x: auto;
     white-space: nowrap;
     scrollbar-width: none; /* Firefox: Hide scrollbar */
   }
-  .question-list::-webkit-scrollbar {
+
+  .inner-wrapper::-webkit-scrollbar {
     display: none; /* Safari and Chrome: Hide scrollbar */
+  }
+
+  .question-list {
+    display: flex;
+    justify-content: center;
+    flex-shrink: 0;
   }
 </style>
 
-<div class="fixed bottom-0 w-full flex justify-center">
-  <div id="question-list" class="question-list px-12 py-2">
-    {#each questions as question}
-      <h1
-        id={`question-${question}`}
-        class="font-sans text-xl text-gray-900 dark:text-gray-200 text-center p-5
-          transition-transform duration-300 ease-in-out cursor-pointer
-          {currentQuestion === question ? 'text-2xl font-bold scale-150' : ''}"
-        on:click={() => questionNumber.set(question)}
-      >
-        {question}
-      </h1>
-    {/each}
+<div class="fixed bottom-0 w-full">
+  <div class="outer-wrapper px-12">
+    <div class="inner-wrapper" id="question-list">
+      {#each questions as question}
+        <h1
+          id={`question-${question}`}
+          class="font-sans text-xl text-gray-900 dark:text-gray-200 text-center p-5
+            transition-transform duration-300 ease-in-out cursor-pointer
+            {currentQuestion === question ? 'text-2xl font-bold scale-150' : ''}"
+          on:click={() => questionNumber.set(question)}
+        >
+          {question}
+        </h1>
+      {/each}
+    </div>
   </div>
 </div>
