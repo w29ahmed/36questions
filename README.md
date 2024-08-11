02c54a13058f46d194d65f5756a30b140b28b89f
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2024-08-10 18:39:27 -0700

    changed welcome screen text

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # 36questions
 
+Inspired by the New York Times article ["36 Questions That Lead to Love"](https://web.archive.org/web/20200812060110/https://www.nytimes.com/2015/01/09/style/no-37-big-wedding-or-small.html), this app guides you through thought-provoking questions to foster intimacy and understanding with a partner or a new acquaintance. Dive in and see where it takes you!
+
+<div align="center">
+  <img src="./assets/demo.gif" alt="demo">
+</div>
+
 ### Built with
 
 - [Svelte](https://svelte.dev): A modern JavaScript framework for building fast, reactive web interfaces
--- src/components/WelcomeScreen.svelte
+++ src/components/WelcomeScreen.svelte
@@ -8,15 +8,14 @@
     <p
       class="font-sans text-md md:text-2xl text-center font-bold text-gray-900 dark:text-gray-200 p-6 mx-4 md:mx-32"
     >
-      Welcome to 36 Questions, inspired by the
-      <a
+      Inspired by the New York Times article "<a
         href="https://web.archive.org/web/20200812060110/https://www.nytimes.com/2015/01/09/style/no-37-big-wedding-or-small.html"
         class="underline"
         target="_blank"
         rel="noopener noreferrer"
       >
-        New York Times article</a
-      >. This app guides you through thought-provoking questions to foster
+        36 Questions That Lead to Love</a
+      >", this app guides you through thought-provoking questions to foster
       intimacy and understanding with a partner or a new acquaintance. Dive in
       and see where it takes you!
     </p>
